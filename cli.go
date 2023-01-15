4e32aa2ad8e98c563ecca254d0dec3fcba019183
shiweifu <shiweifu@gmail.com>
2023-01-15 23:43:19 +0800

    feat:引入cli

---
+++ cli.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/urfave/cli"
+	_ "github.com/urfave/cli/v2"
+	_ "github.com/urfave/cli/v2/altsrc"
+)
+
+func NewCliApp() *cli.NewApp {
+	cliApp := cli.NewApp()
+	cliApp.Name = "markdown-blog"
+	cliApp.Usage = "Markdown Blog App"
+	return cliApp
+}
