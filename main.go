346c63685d4a65f6b9e477b0f3c7b6a6a0e367fb
shiweifu <shiweifu@gmail.com>
2023-01-17 22:49:33 +0800

    feat:完善创建博客日志目录

--- main.go
+++ main.go
@@ -29,9 +29,9 @@ func NewServer() {
 	app.Listen(hostAndPort)
 }
 
-func NewBlogData(path string) error {
+func newBlogDataPages(basePath string) error {
 	// 创建目录
-	pagesPath := path + "/pages"
+	pagesPath := basePath + "/pages"
 	err := os.Mkdir(pagesPath, os.ModePerm)
 	if err != nil {
 		return err
@@ -44,21 +44,88 @@ func NewBlogData(path string) error {
 	if err != nil {
 		return err
 	}
+	// 写入内容
+	_, err = aboutFile.WriteString(AboutPageContent)
+	if err != nil {
+		return err
+	}
+	aboutFile.Close()
+
 	contactFile, err := os.Create(contactPath)
 	if err != nil {
 		return err
 	}
-
-	postsPath := path + "/posts"
-	customPath := path + "/custom"
-	staticPath := path + "/static"
-
-	// 创建文件
-
-	// 写入内容
-
+	_, err = contactFile.WriteString(ContactPageContent)
+	if err != nil {
+		return err
+	}
+	contactFile.Close()
 	return nil
+}
 
+func newBlogDataPosts(basePath string) error {
+	// 创建目录
+	postsPath := basePath + "/posts"
+	err := os.Mkdir(postsPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	// 创建页面文件
+	testPath := postsPath + "/test.md"
+	// 创建文件
+	testFile, err := os.Create(testPath)
+	if err != nil {
+		return err
+	}
+	// 写入内容
+	_, err = testFile.WriteString(PostContent)
+	if err != nil {
+		return err
+	}
+	testFile.Close()
+	return nil
+}
+
+func newBlogDataCustom(basePath string) error {
+	customPath := basePath + "/custom"
+	err := os.Mkdir(customPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	cssPath := customPath + "/custom.css"
+	// 创建文件
+	cssFile, err := os.Create(cssPath)
+	if err != nil {
+		return err
+	}
+	cssFile.Close()
+
+	jsPath := customPath + "/custom.js"
+	// 创建文件
+	jsFile, err := os.Create(jsPath)
+	if err != nil {
+		return err
+	}
+	jsFile.Close()
+	return nil
+}
+
+func NewBlogData(path string) error {
+	err := newBlogDataPages(path)
+	if err != nil {
+		return err
+	}
+
+	err = newBlogDataPosts(path)
+	if err != nil {
+		return err
+	}
+
+	err = newBlogDataCustom(path)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
